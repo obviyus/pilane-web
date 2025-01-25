f20dd74c3f28f84deeab123659ff23b08a836adf
Ayaan Zaidi <zaidi@uplause.io>
2025-01-25 15:24:26 +0530

    refactor: type DB output

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -13,20 +13,42 @@ export function getDb() {
 
 export function getRecentFlights() {
 	const db = getDb();
-	return db
+	const rows = db
 		.query(`
-        SELECT 
-            fr24_id,
-            flight,
-            callsign,
-            type,
-            registration,
-            operator,
-            orig_iata,
-            dest_iata,
-            timestamp
-        FROM flights 
-        ORDER BY timestamp DESC;
-    `)
-		.all();
+		SELECT 
+			fr24_id,
+			flight,
+			callsign,
+			type,
+			registration,
+			operator,
+			orig_iata,
+			dest_iata,
+			timestamp
+		FROM flights 
+		ORDER BY timestamp DESC;
+	`)
+		.all() as {
+		fr24_id: string;
+		flight: string;
+		callsign: string;
+		type: string;
+		registration: string;
+		operator: string;
+		orig_iata: string;
+		dest_iata: string;
+		timestamp: number;
+	}[];
+
+	return rows.map((row) => ({
+		fr24Id: row.fr24_id,
+		flight: row.flight,
+		callsign: row.callsign,
+		type: row.type,
+		registration: row.registration,
+		operator: row.operator,
+		origIata: row.orig_iata,
+		destIata: row.dest_iata,
+		timestamp: row.timestamp,
+	}));
 }
